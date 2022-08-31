015e1e33173cc546d67275d83436017fb664f2b3
Mattias Folkestad <mattias.hallberg@iies.su.se>
2022-08-31 16:15:30 +0200

    Added functinoality
    Added the possibility of two entires per image.

--- toy_func_two_click.py
+++ toy_func_two_click.py
@@ -3,19 +3,14 @@ import tkinter as tk
 import cv2
 import os 
 from PIL import Image, ImageTk
-from pathlib import Path 
+from pathlib import Path
 import re  
 
 # User input 
-name = input("Your name: ")
-name = name.replace(" ","")
-date = input("Date: ")
-date = date.replace(" ","")
 workDir = input("Enter dir: ")
-scaleParam = input("Scale Parameter for Img: ")
-default = input("Choose default value: ")
-scaleParam = float(scaleParam)/100
 nEntries = float(input("Entries per img: "))
+scaleParam = 0.2
+default = "0"
 
 # Directory stuff 
 while Path(workDir).exists() == False:
@@ -28,28 +23,12 @@ while nEntries > 2:
 
 os.chdir(workDir)
 
-filename = workDir + "_" + name + "_" + date + ".csv"
+filename = workDir + ".csv"
 outfile = Path.cwd().parent / filename
 
 # Image files 
 im_files = [f for f in os.listdir(".") if f[-3:] == 'jpg']
 images = iter(im_files)
-
-def set_scale():
-    w.lower(belowThis=label)
-    sp.focus()
-    try:
-        testImg = im_files[0]
-        image = cv2.imread(testImg, flags=cv2.IMREAD_GRAYSCALE)
-        image = cv2.resize(image, (0, 0), fx = scaleParam, fy = scaleParam) # Update to fit the screen
-        img = Image.fromarray(image)
-        imgtk = ImageTk.PhotoImage(image=img)
-        label.imgtk = imgtk
-        label.configure(image=imgtk)
-        T.delete(1.0, tk.END)
-        T.insert(1.0, "Set scale param")
-    except:
-        top.destroy()
 
 def next_img():
     try:
@@ -67,7 +46,6 @@ def next_img():
         T.insert(1.0, imgfile)
     except StopIteration:
         top.destroy()
-    
     
 def func2entries(event, default=default):
     with open(outfile, "r") as f:
@@ -111,21 +89,6 @@ def go_back(event):
     images = iter(im_files[i:])
     next_img()
 
-def done():
-    butt1.lower(belowThis=label)
-    butt2.lower(belowThis=label)
-    sp.lower(belowThis=label)
-    w.lift(aboveThis=label)
-    w.focus()
-    next_img()
-
-def update():
-    global scaleParam
-    scaleParam = sp.get()
-    scaleParam = float(scaleParam) / 100
-    set_scale()
-
-
 # tkinter stuff 
 top = tk.Tk()
 if nEntries == 1:
@@ -144,18 +107,9 @@ T.place(relx = 0.3, rely = 0.1) # PLACE THE FILENAME BOX
 # Entry box 
 w = tk.Entry(top, takefocus=True, font=('calibre',32,'normal'), width=10) #UPDATE FONT SIZE IF NECESSARY
 w.place(relx = 0.2, rely = 0.9) # PLACE THE ENTRY BOX
+w.focus()
 
-# Entry box For scale parameter
-sp = tk.Entry(top , font=('calibre',20,'normal'), width=4 )
-sp.place(relx = 0.4, rely = 0.4)
-
-butt1 = tk.Button(text = "Done", command = done, width= 4)
-butt1.place(relx=0.2, rely=0.8)
-
-butt2 = tk.Button(text = "Rescale", command = update, width= 4)
-butt2.place(relx=0.2, rely=0.4)
-
-set_scale()
+next_img()
 
 top.mainloop()
 
