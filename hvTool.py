b69672ee79a5c7c07f862608e18a6f60a7eb261d
Mattias Folkestad <mattias.hallberg@iies.su.se>
2022-08-31 14:50:37 +0200

    Update hvTool.py

--- hvTool.py
+++ hvTool.py
@@ -1,3 +1,4 @@
+from telnetlib import DM
 import tkinter as tk
 import cv2
 import os 
@@ -11,6 +12,7 @@ date = input("Date: ")
 date = date.replace(" ","")
 workDir = input("Enter dir: ")
 scaleParam = input("Scale Parameter for Img: ")
+default = input("Choose default value: ")
 scaleParam = float(scaleParam)/100
 
 # Directory stuff 
@@ -27,6 +29,22 @@ outfile = Path.cwd().parent / filename
 im_files = [f for f in os.listdir(".") if f[-3:] == 'jpg']
 images = iter(im_files)
 
+def set_scale():
+    w.lower(belowThis=label)
+    sp.focus()
+    try:
+        testImg = im_files[0]
+        image = cv2.imread(testImg, flags=cv2.IMREAD_GRAYSCALE)
+        image = cv2.resize(image, (0, 0), fx = scaleParam, fy = scaleParam) # Update to fit the screen
+        img = Image.fromarray(image)
+        imgtk = ImageTk.PhotoImage(image=img)
+        label.imgtk = imgtk
+        label.configure(image=imgtk)
+        T.delete(1.0, tk.END)
+        T.insert(1.0, "Set scale param")
+    except:
+        top.destroy()
+
 def next_img():
     try:
         global imgfile
@@ -34,7 +52,7 @@ def next_img():
         with open(outfile, "a") as f:
             f.write(imgfile + ";")
         image = cv2.imread(imgfile, flags=cv2.IMREAD_GRAYSCALE)
-        image = cv2.resize(image, (0, 0), fx = scaleParam, fy = scaleParam) # Uppdate to fit the screen
+        image = cv2.resize(image, (0, 0), fx = scaleParam, fy = scaleParam) # Update to fit the screen
         img = Image.fromarray(image)
         imgtk = ImageTk.PhotoImage(image=img)
         label.imgtk = imgtk
@@ -44,12 +62,12 @@ def next_img():
     except StopIteration:
         top.destroy()
     
-def func(event):
-    count = w.get()
-    if count == "":
-        count = "24" 
+def func(event, default=default):
+    entry = w.get()
+    if entry == "":
+        entry = default 
     with open(outfile, "a") as f:
-        f.write(count + "\n")
+        f.write(entry + "\n")
     w.delete(0,"end")
     next_img()
 
@@ -66,6 +84,20 @@ def go_back(event):
     images = iter(im_files[i:])
     next_img()
 
+def done():
+    butt1.lower(belowThis=label)
+    butt2.lower(belowThis=label)
+    sp.lower(belowThis=label)
+    w.lift(aboveThis=label)
+    w.focus()
+    next_img()
+
+def update():
+    global scaleParam
+    scaleParam = sp.get()
+    scaleParam = float(scaleParam) / 100
+    set_scale()
+
 
 # tkinter stuff 
 top = tk.Tk()
@@ -75,16 +107,25 @@ top.bind("<Left>", go_back)
 label = tk.Label(top)
 label.pack()
 
-# Entry box 
-w = tk.Entry(top, takefocus=True, font=('calibre',32,'normal'), width=10) #UPDATE FONT SIZE IF NESSECARY
-w.place(x=180, y=650) # PLACE THE ENTRY BOX
-w.focus()
-
 # Text box
-T = tk.Text(top, font=('calibre',25,'normal'), fg = "red" , height=1, width=20) #UPDATE FONT SIZE IF NESSECARY
-T.place(x = 180, y = 150) # PLACE THE FILENAME BOX
+T = tk.Text(top, font=('calibre',25,'normal'), fg = "red" , height=1, width=20) #UPDATE FONT SIZE IF NECESSARY
+T.place(relx = 0.3, rely = 0.1) # PLACE THE FILENAME BOX
 
-next_img()
+# Entry box 
+w = tk.Entry(top, takefocus=True, font=('calibre',32,'normal'), width=10) #UPDATE FONT SIZE IF NECESSARY
+w.place(relx = 0.2, rely = 0.9) # PLACE THE ENTRY BOX
+
+# Entry box For scale parameter
+sp = tk.Entry(top , font=('calibre',20,'normal'), width=4 )
+sp.place(relx = 0.4, rely = 0.4)
+
+butt1 = tk.Button(text = "Done", command = done, width= 4)
+butt1.place(relx=0.2, rely=0.8)
+
+butt2 = tk.Button(text = "Rescale", command = update, width= 4)
+butt2.place(relx=0.2, rely=0.4)
+
+set_scale()
 
 top.mainloop()
 
